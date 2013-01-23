1b7d37b7c01b7f8e31d8d2a4333dd112360f6154
Guest User <Guest@Mac110-Golgari.local>
2013-01-23 10:49:53 -0500

    Got Hello portion of code working.

---
+++ GUI Demo/GUI Demo/GDAppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  GDAppDelegate.m
+//  GUI Demo
+//
+//  Created by Guest User on 1/23/13.
+//  Copyright (c) 2013 Guest User. All rights reserved.
+//
+
+#import "GDAppDelegate.h"
+
+@implementation GDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+- (IBAction)doHello:(id)sender {
+    NSString* currentText = nil;
+    currentText = [[self theTextField] stringValue];
+    currentText = [NSString stringWithFormat: @"Hello, %@",    currentText ];
+    [[self theTextField] setStringValue: currentText];
+
+}
+
+- (IBAction)doGoodbye:(id)sender {
+}
+@end
